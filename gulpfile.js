ae0baf3f3849abbf6abf0069bbe9ac0177ce6709
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-12-09 22:45:28 +0300

    New gulp tasks. Added jquery.

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,49 @@ const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 
 
-/* Styles
+/* Dev
 ==================== */
-gulp.task('styles:less', function() {
+
+/* Style */
+gulp.task('dev:styles', function() {
 
   return gulp.src('./src/less/styles.less')
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./public/css'))
+});
+
+/* js */
+gulp.task('dev:js', function(cb) {
+  return gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./public/js'));
+});
+
+/* Images */
+gulp.task('dev:images', function() {
+  
+  return gulp.src('./src/images/*')
+    .pipe(gulp.dest('./public/images'));
+});
+
+/* Entry */
+gulp.task('dev:entry', function() {
+
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./public'));
 });
 
 
-gulp.task('styles:min', function() {
+/* Prod
+======================== */
+
+/* Style */
+gulp.task('prod:styles', function() {
   
-  return gulp.src('./dist/css/styles.css')
+  return gulp.src('./src/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist/css'))
     .pipe(cleanCSS())
     .pipe(rename({
       basename: 'styles',
@@ -34,21 +62,14 @@ gulp.task('styles:min', function() {
 });
 
 
-gulp.task('build:styles', gulp.series(
-  'styles:less',
-  'styles:min'
-));
-
-
-/* JS
-========================= */
-gulp.task('js:js', function(cb) {
+/* js */
+gulp.task('prod:jsCp', function(cb) {
   return  gulp.src('./src/js/*.js')
     .pipe(gulp.dest('./dist/js'));
 });
 
 
-gulp.task('js:min', function(cb) {
+gulp.task('prod:min', function(cb) {
   pump([
       gulp.src('./src/js/*.js'),
       uglify(),
@@ -61,31 +82,61 @@ gulp.task('js:min', function(cb) {
   );
 });
 
-
-gulp.task('build:js', gulp.series(
-  'js:js',
-  'js:min'
+gulp.task('prod:js', gulp.series(
+  'prod:jsCp',
+  'prod:min'
 ));
 
 
-/* Images
-================================== */
-gulp.task('build:images', function() {
+/* Images */
+gulp.task('prod:images', function() {
 
   return gulp.src('./src/images/*')
     .pipe(gulp.dest('./dist/images'));
 });
 
 
-// Clean dist directory
-gulp.task('build:clean', function() {
+/* Clean directories 
+============================*/
 
+/* Dev */
+gulp.task('dev:clean', function() {
+
+  return del('./public');
+});
+
+/* Prod */
+gulp.task('prod:clean', function() {
+  
   return del('./dist');
 });
 
 
-// Build
+/*  Build
+========================== */
+
+/* Watch */
+gulp.task('watch', function() {
+  gulp.watch('./src/less/**/*.less', gulp.series('dev:styles')); // watch styles
+  gulp.watch('./src/js/**/*.js', gulp.series('dev:js')); // watch js files
+  gulp.watch('./src/images/**/*.*', gulp.series('dev:images')); // watch imgs
+});
+
+/* Dev build*/
+gulp.task('dev:build', gulp.series(
+  'dev:clean', 
+  gulp.parallel('dev:styles', 'dev:images', 'dev:js', 'dev:entry')
+));
+
+/* Dev  build + watch */
+gulp.task('dev', gulp.series(
+  'dev:build',
+  'watch'
+));
+
+
+/* Prod */
 gulp.task('build', gulp.series(
-  'build:clean', 
-  gulp.parallel('build:styles', 'build:images', 'build:js')
+  'prod:clean', 
+  gulp.parallel('prod:styles', 'prod:images', 'prod:js')
 ));
